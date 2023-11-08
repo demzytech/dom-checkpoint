e13446a6aa98234e62843b0e9e373d7cc56289fd
demzytech <demiladeamodu@gmail.com>
2023-11-08 14:03:40 +0100

    adding text

--- styles/script/main.js
+++ styles/script/main.js
@@ -1,11 +1,5 @@
 console.log("javascript is connected...");
 let add = document.getElementById('plus')
 function add (addition) {
-    let add = 0;
-while(add< 100) {
-    if (add % 2 === 0) {
-        console.log("Even Num =", add)
-    }
-    // increment
-    add++;
+    let plus = document.getElementsByClassName('quantity')
 }
